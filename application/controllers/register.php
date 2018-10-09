d17c7955d38f79d0b2aabca38c7eb59f8ac8fdcd
unknown <cabuenasjayzon@gmail.com>
2018-10-09 20:17:56 +0800

    updates

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -8,13 +8,20 @@ class Register extends CI_Controller {
 			$this->load->view('dashboard/index');
 	}
 	public function savedata() {
-		$this->form_validation->set_rules('lname', 'Last Name', 'trim|required');
-		$this->form_validation->set_rules('fname', 'First Name', 'trim|required');
+		$this->form_validation->set_rules('lname', 'Last Name', 'trim');
+		$this->form_validation->set_rules('fname', 'First Name', 'trim');
 		$this->form_validation->set_rules('username', 'Username', 'required');
 		$this->form_validation->set_rules('password', 'Password', 'required');
 		$this->form_validation->set_rules('confirm_password', 'Confirm Password', 'required');
-		if ($this->form_validation->run()) {
-			print_r($_POST);
+		if ($this->form_validation->run()){
+			$data = array(
+				'lname' => $this->input->post('lname'),
+				'fname' => $this->input->post('fname'),
+				'username' => $this->input->post('username'),
+				'password' => $this->input->post('password')
+			);
+			$this->db->insert('register', $data);
+
 		} else 
 			redirect('register','refresh');
 	}
