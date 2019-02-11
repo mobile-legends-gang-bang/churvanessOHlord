3578db9381b44a1f5b9bff74469f4f82cb211bf6
stelalicante <alicantekarystel@gmail.com>
2019-02-11 16:31:08 +0800

    a

--- application/views/attendance/index.php
+++ application/views/attendance/index.php
@@ -20,9 +20,10 @@
       </ul>
     </div>
 
+
     <div class="tab-content" id="pills-tabContent">
+
     <div class="tab-pane fade show active" id="checkattendance" role="tabpanel" aria-labelledby="check-attendance-tab">
-    <div class="container-fluid">
       <!-- Example DataTables Card-->
       <div class="row" style="padding: 20px;">
       	<div style="padding-right: 20px; padding-top: 5px;">Date</div>
@@ -128,10 +129,30 @@
         </div>
       </div>
     </div>
-    </div>
-
-      <div class="tab-pane fade show active" id="seatplan" role="tabpanel" aria-labelledby="seat-plan-tab">
-        <div style="padding-right: 20px; width: 50px; padding-top: 5px; padding-left: 10px;">Class Section</div>
+   
+      <div class="tab-pane fade" id="seatplan" role="tabpanel" aria-labelledby="seat-plan-tab">
+          <div class="row" style="padding: 20px;">
+        <div style="padding-right: 20px; padding-top: 5px;">Date</div>
+        <div>
+          <input name="" class="form-control" value="<?php echo date('m/d/Y')?>" disabled>
+        </div>
+        <div style="padding-right: 20px; padding-top: 5px;">Subject Select</div>
+        <div>
+          <select class="form-control">
+          <optgroup>
+            <option>
+              Social Science
+            </option>
+            <option>
+              Mathematics
+            </option>
+            <option>
+              English
+            </option>
+          </optgroup>
+        </select>
+        </div>
+        <div style="padding-right: 10px;  padding-top: 5px; padding-left: 10px;">Class Section</div>
         <div>
           <select class="form-control">
           <optgroup>
@@ -147,6 +168,92 @@
           </optgroup>
         </select>
         </div>
+        <div class="container">
+          <div class="row" style="margin-left: 437px;">
+         <div class="cell">Teacher</div>
+       </div>
+    <div class="row">
+        <div class="cell">1
+          <select>
+            <option>
+              Jayzon Cabuenas
+            </option>
+            <option>
+              Karystel Alicante
+            </option>
+          </select></div>
+        <div class="cell">2</div>
+        <div class="cell">3</div>
+        <div class="cell">4</div>
+        <div class="cell">5</div>
+        <div class="cell">6</div>
+        <div class="cell">7</div>
+        <div class="cell">8</div>
+    </div>
+    <div class="row">
+        <div class="cell">1</div>
+        <div class="cell">2</div>
+        <div class="cell">3</div>
+        <div class="cell">4</div>
+        <div class="cell">5</div>
+        <div class="cell">6</div>
+        <div class="cell">7</div>
+        <div class="cell">8</div>
+    </div>
+    <div class="row">
+        <div class="cell">1</div>
+        <div class="cell">2</div>
+        <div class="cell">3</div>
+        <div class="cell">4</div>
+        <div class="cell">5</div>
+        <div class="cell">6</div>
+        <div class="cell">7</div>
+        <div class="cell">8</div>
+    </div>
+      <div class="row">
+        <div class="cell">1</div>
+        <div class="cell">2</div>
+        <div class="cell">3</div>
+        <div class="cell">4</div>
+        <div class="cell">5</div>
+        <div class="cell">6</div>
+        <div class="cell">7</div>
+        <div class="cell">8</div>
+    </div>
+      <div class="row">
+        <div class="cell">1</div>
+        <div class="cell">2</div>
+        <div class="cell">3</div>
+        <div class="cell">4</div>
+        <div class="cell">5</div>
+        <div class="cell">6</div>
+        <div class="cell">7</div>
+        <div class="cell">8</div>
+    </div>
+  </div>
+</div>
+</div>
+<style type="text/css">
+.container{
+  margin-top: 40px; 
+  align-self: center;
+}
+.row {
+    display: flex;
+}
+.cell {
+    background: #98FB98;
+    color:  white;
+    width: 130px;
+    height: 80px;
+    flex: 0 0 auto;
+    text-align: center;
+    padding: 12px;
+    margin: 5px;
+    border-right: 3px solid #fff;
+    border-bottom: 3px solid #fff;
+}
+</style>
       </div>
       </div>
     <!-- Scroll to Top Button-->
@@ -155,3 +262,4 @@
     </a>
   </div>
  </body>
+ </html>
