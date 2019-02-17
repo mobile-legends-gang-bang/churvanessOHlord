6f14a88dc2c16c0dc7b6e483147dc36391d279e8
unknown <cabuenasjayzon@gmail.com>
2019-02-18 04:52:00 +0800

    jayzon

--- application/views/student_record/records.php
+++ application/views/student_record/records.php
@@ -2,12 +2,10 @@
     <tr><td colspan="14" align="center">No record(s) found!</td></tr>
 <?php else: ?>
     <?php foreach ($records->result() as $row):?>
-        <?php $count = $row->count_absent/5;?>
         <tr>
-            <td><?php echo $row->s_id ?></td>
+            <td style="width: 120px; text-align: right;"><?php echo $row->s_id ?></td>
             <td><?php echo $row->lname.", ".$row->fname." ".$row->mname." ".$row->extname?></td>
             <td><?php echo $row->count_absent?></td>
-            <td><?php echo $count?></td>
         </tr>
     <?php endforeach;?>
 <?php endif; ?>
