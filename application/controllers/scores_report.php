5a1dd25590e2626cd081dcd091a4058ff8a01183
zeussale <zeussale@gmail.com>
2019-02-20 23:59:53 +0800

    scores report done

--- application/controllers/scores_report.php
+++ application/controllers/scores_report.php
@@ -33,8 +33,66 @@ class Scores_report extends CI_Controller {
     //   $data["employee_data"] = $this->excel_export_model->fetch_data();
     //   $this->load->view("excel_export_view", $data);
     //  }
+    public function action() {
+        // $this->load->model("scores_report_model");
+        // $object = new PHPExcel();
+        // $object->setActiveSheetIndex(0);
 
-    public function action(){
+        $this->excel->setActiveSheetIndex(0);
+        $spreadsheet = $this->excel->getActiveSheet();
+
+        $header = array();
+        $header[] = 'Student ID';
+        $header[] = 'Name';
+        $header[] = 'Score Type';
+        $header[] = 'Score';
+        $header[] = 'Total Score';
+
+        $spreadsheet->setCellValue('A1', "Score's Report");
+
+        $spreadsheet->setCellValue('A3', $header[0]);
+        $spreadsheet->setCellValue('B3', $header[1]);
+        $spreadsheet->setCellValue('C3', $header[2]);
+        $spreadsheet->setCellValue('D3', $header[3])->mergeCells('D3:M3');
+        $spreadsheet->setCellValue('N3', $header[4]);
+
+        $cell = 4;
+        $records = $this->scores_report_model->getscores();
+        foreach ($records->result() as $row) {
+            $spreadsheet->setCellValue('A'.$cell, $row->s_id);
+            $spreadsheet->setCellValue('B'.$cell, $row->lname.", ".$row->fname." ".$row->mname);
+            $spreadsheet->setCellValue('C'.$cell, $row->score_type);
+            // $object->getActiveSheet()->setCellValue('D'.$cell, $row->score_sum);
+
+            $maxLength = 10;
+            $scores = explode(' - ', $row->scores);
+            $scoresLength = count($scores);
+            $column = 'D';
+            $column++;
+            for($i = 0; $i < $scoresLength; $i++) {
+                $spreadsheet->setCellValue($column.$cell, $scores[$i]);
+                $column++;
+            }
+            for($i = $scoresLength; $i < $maxLength; $i++) {
+                $spreadsheet->setCellValue($column.$cell, '0');
+                $column++;
+            }
+
+            // $object->getActiveSheet()->setCellValue('D3', $header[3])->mergeCells('D3:M3');
+            $spreadsheet->setCellValue('N'.$cell, $row->score_sum);
+            $cell++;
+        }
+
+       $filename='scores.xls';
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment;filename="'.$filename.'"');
+        header('Cache-Control: max-age=0'); //no cache
+                     
+        $objWriter = PHPExcel_IOFactory::createWriter($this->excel, 'Excel5');  
+        $objWriter->save('php://output');
+    }
+
+    public function action2(){
         $this->load->model("scores_report_model");
         $object = new PHPExcel();
         $object->setActiveSheetIndex(0);
